d1e4a398b63f3a075c9a1c2540fd5b613a833a84
A-Moeed-B <abdul_moeed123@outlook.com>
2020-07-02 17:22:56 +0500

    header file

--- ManagementSystem/Person.h
+++ ManagementSystem/Person.h
@@ -15,5 +15,41 @@ protected:
 	string password;
 	bool signIn;
 public:
-	person()
+	Person();
+	
+		/*name = "N/A";
+		fName = "N/A";
+		age = 0;
+		cnic = "N/A";
+		gender = "N/A";
+		id = "N/A";
+		password = "N/A";*/
+	
+	Person(string name, int age, string fName, string cnic, string gender, string password, string id);
+	
+		/*this->name = name;
+		this->age = age;
+		this->fName = fName;
+		this->cnic = cnic;
+		this->gender = gender;
+		this->password = password;
+		this->signIn = signIn;*/
+	
+	void setName(string name);
+	
+	void setAge(int age);
+	void setCnic(string cnic);
+	void setFname(string fName);
+	void setGender(string gender);
+	void setId(string id);
+	void setPassword(string password);
+	string getName();
+	string getFName();
+	string getGender();
+	string getID();
+	string getPassword();
+	string getAge();
+	void showData();
+	virtual void signIn() = 0;
+	void changePass();
 };
