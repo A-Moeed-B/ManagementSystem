cd406e902354a3778df3ed56a548954d6e91d162
A-Moeed-B <abdul_moeed123@outlook.com>
2020-07-02 17:06:46 +0500

    adding new

---
+++ ManagementSystem/Person.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class  Person
+{
+protected:
+	string name;
+	int age;
+	string fName;
+	string cnic;
+	string gender;
+	string id;
+	string password;
+	bool signIn;
+public:
+	person()
+};
